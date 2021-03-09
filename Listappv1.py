d432531e465ddfe86eb29d8182ab60503cdb7f9f
StarFc <79867299+StarFc@users.noreply.github.com>
2021-03-09 10:22:18 -0800

    Add files via upload

---
+++ Listappv1.py
@@ -0,0 +1,30 @@
+"'"
+Program goals:
+1. Add item to a list (ints)
+2. Offer the user a choice of actions
+3. Pull the values stored at specific indexes
+
+"""
+
+def mainProgram():
+    myList = []
+    print("Hello, there! Let's work with lists!")
+    print("Please choose front the following options. Type the number of the cboice")
+    choice = input("1. Add to a list, 2. Return a value at a list    ")
+    if choice == 1":
+        addToList()
+    elif choice == "2":
+        indexValues()
+
+def addToList():
+    print("Adding to a list! Great choice!")
+    newItem = input(Type an integer here!    ")
+    myList.append(int(newItem))
+    #we need to think about errors!
+def indexValues():
+
+
+
+
+if__name__=="__main__":
+    mainProgram()
